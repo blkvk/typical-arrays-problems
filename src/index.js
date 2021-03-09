0e30af046c5fabe904407f8bfd729913c417bdfa
Kirill Beliakov <kabeliakov@gmail.com>
2021-03-09 20:25:07 +0300

    feat: add typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array == null) {
+    return 0;
+  };
+  if (array.length == 0) {
+    return 0;
+  };
+  let min = Infinity;
+  for (let i=0; i < array.length; i++){
+    if(array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array == null) {
+    return 0;
+  };
+  if (array.length == 0) {
+    return 0;
+  };
+  let max = !Infinity;
+  for (let i=0; i < array.length; i++){
+    if(array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array == null) {
+    return 0;
+  };
+  if (array.length == 0) {
+    return 0;
+  };
+  let sum = 0;
+  let length = array.length;
+  for (let i=0; i < length; i++){
+    sum += array[i];
+  }
+  let ans = sum / length;
+  return ans;
+};
